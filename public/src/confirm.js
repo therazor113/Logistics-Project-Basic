c772af9dbc7a287cccc377feb2e807676d5acbd2
Razor <therazor113@gmail.com>
2022-06-07 12:34:52 -0400

    cleaner code

--- public/src/confirm.js
+++ public/src/confirm.js
@@ -13,5 +13,5 @@ const checkbox = document.querySelector('.checkbox')
 checkbox.addEventListener('mousedown', () => {
   check = check === false ? true : false
   checkbox.textContent = check === true ? '' : 'X'
-  checkbox.style.backgroundColor = check === true ? 'skyblue' : 'yellow'
+  checkbox.style.backgroundColor = check === true ? 'skyblue' : 'orange'
 })
